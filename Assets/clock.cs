3ca9c3eeae06d395bd309f41e203504f97b65790
Jake Simon <js961214@ohio.edu>
2017-02-02 13:21:39 -0500

    Commit Two
    still doesn't work Unity hates me

---
+++ Assets/clock.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Quaternion euler = Quaternion.Euler (
+		   new Vector3 (0f, 0f, Time.time * 360f));
+		transform.localRotation = euler;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
